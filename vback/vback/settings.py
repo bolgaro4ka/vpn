313afc6d11d7907df4cfb271b8c1dc90a8d4e0af
Nikita <fedosovnikita007@gmail.com>
2024-10-20 21:25:16 +0300

    Добавил в AH

--- vback/vback/settings.py
+++ vback/vback/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-ys&5unzw*@hpeykt#b6ortwrt)u0(5gb$dc3f7yi@yvv#5j=q@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.228', '10.66.66.5', 'localhost', '212.3.131.59', 'vpn.paia1nik.ru']
+ALLOWED_HOSTS = ['192.168.0.228', '10.66.66.5', 'localhost', '212.3.131.59', 'vpn.paia1nik.ru', 'vpna.paia1nik.ru']
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.0.228:4000",  # Замените на ваш фронтенд домен
     'http://10.66.66.5:4000',
